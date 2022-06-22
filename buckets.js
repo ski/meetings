1400db08bc26221323c28352ad355c23c9c71950
Suhail Manzoor <suhailski@gmail.com>
2022-06-22 09:08:23 +0530

    proxy agent

--- buckets.js
+++ buckets.js
@@ -35,7 +35,7 @@ const token = jwt.sign(payload, Config.APISecret);
 
 async function request(i, endpoint) {
   const response =  fetch(endpoint, {
-    //agent: proxyAgent,
+    agent: proxyAgent,
     method: "get",
     headers: {
       "Content-Type": "application/json",
